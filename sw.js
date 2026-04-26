6b941087be7a308fb96b31891cbc1651dc7cd967
João Henrique De Souza Borges <109562443+JoaoH293@users.noreply.github.com>
2026-04-26 11:14:49 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE_NAME = 'biblia-app-v1';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html', 'style.css'])));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
